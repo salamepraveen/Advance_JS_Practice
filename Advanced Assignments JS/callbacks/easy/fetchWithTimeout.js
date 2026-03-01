67ac018912d01c9831a7ad0d91e83b45104ccf61
KyojoruRengoku <praveensdarc@gmail.com>
2026-03-01 23:08:58 +0530

    Started practicing more types of Callbacks,promises,cpu io:

---
+++ Advanced Assignments JS/callbacks/easy/fetchWithTimeout.js
@@ -0,0 +1,20 @@
+// Problem Description – fetchWithTimeout(url, ms, callback)
+//
+// You are required to write a function named fetchWithTimeout that accepts a URL,
+// a time limit in milliseconds, and a callback function.
+// The function attempts to fetch data from the given URL.
+// If the request completes within the specified time, the callback is invoked with
+// null as the first argument and the fetched data as the second argument.
+// If the operation exceeds the time limit, the callback is invoked with an Error
+// whose message is "Request Timed Out".
+
+const fs=require("fs")
+
+function fetchWithTimeout(url, ms, callback) {
+       fetch(url).then
+        return setTimeout(()=>{
+                callback(null,url)
+        },ms)
+}
+
+module.exports = fetchWithTimeout;
