647345f6798b3769b7ea9e16af87ff55e914bc36
salamepraveen <salamepraveen15@gmail.com>
2026-03-27 22:12:06 +0530

    Todo App

--- week9/index.js
+++ week9/index.js
@@ -2,6 +2,41 @@ const express=require("express")
 const app=express();
 app.use(express.json())
 const notes=[]; // this is bad practice, its an In memory variable which will help us store data
+const users=[{
+    username:"prav",
+    password:"123123"
+},{
+    username:"Spidey",
+    password:"Gwen124"
+}]
+
+app.post("/signup",function(req,res){
+    const username=req.body.username;
+    const password=req.body.passwod;
+    const userExists=users.find(user=>user.username==username);
+    if(userExists){
+        return res.status(403).json({
+            message:"User with this username already exists"
+        })
+    }
+
+    users.push({
+        username:username,password:password
+});
+
+res.json({
+    message:"You have signedUp"
+})
+})
+
+
+app.post("/sign",function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+    const userExist=
+})
+
 
 // Post - create a note
 app.post("/notes",function(req,res){
